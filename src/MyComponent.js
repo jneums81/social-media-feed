29e6b6fd5d68ca9bef66dbc63c67541e06eae0f4
Jackson Neuman <neumanjackson@gmail.com>
2024-01-23 22:03:56 -0600

    added CSS

---
+++ src/MyComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MyComponent = () => {
+  // Define styles as a JavaScript object
+  const myStyle = {
+    color: 'blue',
+    fontSize: '16px',
+    // Add more styles as needed
+  };
+
+  return (
+    <div style={myStyle}>
+      The Social
+    </div>
+  );
+};
+
+export default MyComponent;
